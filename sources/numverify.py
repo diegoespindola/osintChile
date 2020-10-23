8a98f5592a968e4cc32056714b375261246cc084
Diego Espindola <diego.espindola@gmail.com>
2020-10-23 11:31:12 -0300

    Update numverify.py
    se cambia key de api limitada a 250 querys por api scrapeada desde home de numverify

--- sources/numverify.py
+++ sources/numverify.py
@@ -2,38 +2,54 @@ import argparse
 import requests
 import json
 import codecs
+from bs4 import BeautifulSoup
+import hashlib
 
 def busqueda(telefono):
     busquedaNumVerify(telefono)
 
 def busquedaNumVerify(telefono):
-    urlNumverifyRoted = 'uggc://ncvynlre.arg/ncv/inyvqngr?sbezng=1&npprff_xrl=3888s896p1779qsn549o4o9r1pp71995&ahzore='
 
-    urlNumverify = codecs.decode(urlNumverifyRoted, 'rot_13')  + telefono
-    print(' ')
-    print('  --==< Datos desde NumVerify.com >==--')
+    urlNumVrfyHomeRoted = 'uggc://ahzirevsl.pbz/'
+    urlNumVrfyHome = codecs.decode(urlNumVrfyHomeRoted, 'rot_13')
 
-    page = requests.get(url=urlNumverify)
-    if page.status_code==200:
-        if ( page.text != 'Unauthorized'):
-            datosJson = json.loads(page.text)
-            if 'success' in datosJson and datosJson['success']== False :
-                    print('  ' + datosJson['error']['info'])
-            else:
-                if 'valid' in  datosJson and datosJson['valid'] == True:
-                    print('     Numero                  :', datosJson['number'])
-                    print('     Formato internacional   :', datosJson['international_format'])
-                    print('     Codigo de Pais          :', datosJson['country_code'])
-                    print('     Pais                    :', datosJson['country_name'])
-                    print('     Carrier                 :', datosJson['carrier'])
-                    print('     Tipo de linea           :', datosJson['line_type'])
-                    print(' ')
+    pageKey = requests.get(url=urlNumVrfyHome)
+    if pageKey.status_code == 200:
+        soup = BeautifulSoup(pageKey.content, 'html.parser')
+        secret_key = soup.find_all('input',type='hidden')[1]['value']
+
+        concat=telefono + secret_key
+        api_key = hashlib.md5( concat.encode('utf-8')).hexdigest()
+
+        urlNumverifyRoted = 'uggcf://ahzirevsl.pbz/cuc_urycre_fpevcgf/cubar_ncv.cuc?frperg_xrl='
+
+        urlNumverify = codecs.decode(urlNumverifyRoted, 'rot_13') + api_key + '&number=' + telefono
+        print(' ')
+        print('  --==< Datos desde NumVerify.com>==--')
+
+        page = requests.get(url=urlNumverify)
+        if page.status_code==200:
+            if ( page.text != 'Unauthorized'):
+                datosJson = json.loads(page.text)
+                if 'success' in datosJson and datosJson['success']== False :
+                        print('  ' + datosJson['error']['info'])
                 else:
-                    print('  Telefono invalido')
+                    if 'valid' in  datosJson and datosJson['valid'] == True:
+                        print('     Numero                  :', datosJson['number'])
+                        print('     Formato internacional   :', datosJson['international_format'])
+                        print('     Codigo de Pais          :', datosJson['country_code'])
+                        print('     Pais                    :', datosJson['country_name'])
+                        print('     Carrier                 :', datosJson['carrier'])
+                        print('     Tipo de linea           :', datosJson['line_type'])
+                        print(' ')
+                    else:
+                        print('  Telefono invalido')
+            else:
+                print(datosJson['  No autorizado'])
         else:
-            print(datosJson['  No autorizado'])
+            print("  Error en request:", page.status_code)          
     else:
-        print("  Error en request:", page.status_code)            
+        print("  Error en request:", pageKey.status_code)                        
 
 if __name__ == "__main__":
     print('Esto no se ejecuta solo, es para ser llamado desde el programa principal')
